885b16384ef9bbd3b621eab641436c64866e7098
Ezeokeke Kingsley <kezeokeke@seamfix.com>
2018-04-11 19:50:03 +0100

    consume service to fetch plans

---
+++ src/app/pages/app-content/services/subscription.service.ts
@@ -0,0 +1,36 @@
+/**
+ * Created by Kingsley Ezeokeke on 4/10/2018.
+ */
+
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+import {Endpoints} from "../../../util/endpoints";
+import {MediaType} from "../../../util/constants";
+import {map, timeout} from "rxjs/operators";
+import {AuthService} from "../../../components/auth/auth.service";
+
+@Injectable()
+export class SubscriptionService {
+
+    constructor(private httpClient: HttpClient, private as: AuthService) {}
+
+
+    fetchPlans():Observable<any> {
+
+        return this.httpClient
+            .get(Endpoints.FETCH_SUBSCRIPTION_PLANS, {
+                headers: new HttpHeaders()
+                    .set('Content-Type', MediaType.APPLICATION_FORM_URLENCODED)
+            })
+            .pipe(
+                timeout(50000),
+                map(response => {
+                    let res:any = response;
+                    this.as.checkUnauthorized(res.description);
+                    return res
+                })
+            )
+    }
+
+}
--- src/app/pages/app-content/subscribe/subscribe.component.ts
+++ src/app/pages/app-content/subscribe/subscribe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {SubscriptionPlan} from "../model/app-content.model";
+import {SubscriptionService} from "../services/subscription.service";
+import {NotifyService} from "../../../service/notify.service";
 
 @Component({
   selector: 'app-subscribe',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscribeComponent implements OnInit {
 
-  constructor() { }
+  public subscriptionPlans: SubscriptionPlan[] = [
+      {name: "BASIC", pricePerMonth: 30000, maxAttendeeThreshold: 900, description: null, pricePerAnnum: 90000, priceperDay: 400, enabled:true},
+      {name: "PREMIUM", pricePerMonth: 50000, maxAttendeeThreshold: 900, description: null, pricePerAnnum: 90000, priceperDay: 400, enabled:true}
+      ];
+
+  constructor(private subService: SubscriptionService, private ns: NotifyService) { }
 
   ngOnInit() {
+      this.fetchPlans();
+  }
+
+  fetchPlans() {
+    this.subService.fetchPlans()
+        .subscribe(
+            result => {
+                if(result.code == 0) {
+                    this.subscriptionPlans = result.plans? result.plans: [];
+                }else {
+                    this.ns.showError(result.description);
+                }
+            },
+            error => {this.ns.showError("An Error Occurred")}
+        )
   }
 
 }
