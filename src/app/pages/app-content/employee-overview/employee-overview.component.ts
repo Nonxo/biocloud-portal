89dccb418bd4877b720f5c688e89e6017d237591
kingsley ezeokeke <kezeokeke@seamfix.com>
2018-07-11 08:01:14 +0100

    consume service to get employees clockin stats

--- src/app/pages/app-content/employee-overview/employee-overview.component.ts
+++ src/app/pages/app-content/employee-overview/employee-overview.component.ts
@@ -4,8 +4,10 @@ import {Router} from "@angular/router";
 import {AppContentService} from "../services/app-content.service";
 import {NotifyService} from "../../../service/notify.service";
 import {DataService} from "../../../service/data.service";
-import {HistoryPojo, UpdateProfile} from "../model/app-content.model";
+import {DaysPresentRequest, HistoryPojo, UpdateProfile} from "../model/app-content.model";
 import {isNullOrUndefined} from "util";
+import {ReportService} from "../services/report.service";
+import {DateUtil} from "../../../util/DateUtil";
 
 @Component({
     selector: 'app-employee-overview',
@@ -23,12 +25,24 @@ export class EmployeeOverviewComponent implements OnInit, OnDestroy {
     punctualityScore:number = 0;
     active:string = "poor";
     locations:any[] = [];
+    daysPresentRequest: DaysPresentRequest = new DaysPresentRequest();
+    totalDaysPresent: any[] = [{title: "This week"},{title: "This Month"},{title: "This year"}];
+    totalDaysEarly: any[] = [{title: "This week"},{title: "This Month"},{title: "This year"}];
+    totalDaysLate: any[] = [{title: "This week"},{title: "This Month"},{title: "This year"}];
+    totalDaysAbsent: any[] = [{title: "This week"},{title: "This Month"},{title: "This year"}];
+    selectedRangeForDaysPresent: any;
+    selectedRangeForDaysEarly: any;
+    selectedRangeForDaysLate: any;
+    startRange: number = 1525190429000;
+    endRange: number = 1531245317000;
 
     constructor(private mService: MessageService,
                 private contentService: AppContentService,
                 private router: Router,
                 private ns: NotifyService,
-                private dataService: DataService) {
+                private dataService: DataService,
+                private reportService: ReportService,
+                private dateUtil: DateUtil) {
         this.mService.setTitle("Employee Overview");
     }
 
@@ -42,6 +56,8 @@ export class EmployeeOverviewComponent implements OnInit, OnDestroy {
             this.fetchUserDetail();
             this.fetchClockinHistory();
             this.fetchPuncScore();
+            this.fetchDaysPresent();
+            // this.getAverageTime();
         }
 
     }
@@ -119,6 +135,118 @@ export class EmployeeOverviewComponent implements OnInit, OnDestroy {
                 },
                 error => {}
             )
+    }
+
+    fetchDaysPresent() {
+        this.fetchWeeklyStat();
+        // this.fetchMonthlyStat();
+
+        //fetch current month
+
+        //fetch current year
+    }
+
+    getAverageTime() {
+        this.daysPresentRequest.email = this.userObj.email;
+        this.daysPresentRequest.orgId = this.userObj.orgId;
+        this.daysPresentRequest.locId = this.userObj.locId;
+        this.daysPresentRequest.currentStartTime = this.dateUtil.getStartOfDay(new Date(this.startRange));
+        this.daysPresentRequest.currentEndTime = this.dateUtil.getEndOfDay(new Date(this.endRange));
+        this.daysPresentRequest.prevStartTime = this.dateUtil.getPreviousWeekTimeStamp(this.dateUtil.getStartOfDay(new Date(this.startRange)) - 1)
+        this.daysPresentRequest.prevEndTime = this.dateUtil.getStartOfDay(new Date(this.startRange)) - 1;
+
+
+        this.reportService.getAvgTime(this.daysPresentRequest)
+            .subscribe(
+                result => {debugger;},
+                error => {debugger;}
+            )
+    }
+
+    fetchWeeklyStat() {
+        let startRange = this.dateUtil.getFirstDayOfCurrentWeek(new Date()).getTime();
+        let endRange = this.dateUtil.getLastDayOfCurrentWeek(new Date()).getTime();
+
+        //fetch current week
+        this.daysPresentRequest.email = this.userObj.email;
+        this.daysPresentRequest.orgId = this.userObj.orgId;
+        this.daysPresentRequest.locId = this.userObj.locId;
+        this.daysPresentRequest.currentStartTime = this.dateUtil.getStartOfDay(new Date(startRange));
+        this.daysPresentRequest.currentEndTime = this.dateUtil.getEndOfDay(new Date(endRange));
+        this.daysPresentRequest.prevStartTime = this.dateUtil.getPreviousWeekTimeStamp(this.dateUtil.getStartOfDay(new Date(startRange)) - 1)
+        this.daysPresentRequest.prevEndTime = this.dateUtil.getStartOfDay(new Date(startRange)) - 1;
+        this.reportService.getDaysPresent(this.daysPresentRequest)
+            .subscribe(
+                result => {
+                    this.totalDaysPresent[0].daysPresent = result.daysPresent;
+                    // this.totalDaysPresent[0].daysPresent = result.daysPresent;
+                    this.totalDaysEarly[0].daysEarly = result.daysEarly;
+                    this.totalDaysEarly[0].trend = result.earlyTrend;
+                    this.totalDaysLate[0].daysLate = result.daysLate;
+                    this.totalDaysLate[0].trend = result.lateTrend;
+
+                    this.selectedRangeForDaysPresent = this.totalDaysPresent[0];
+                    this.selectedRangeForDaysEarly = this.totalDaysEarly[0];
+                    this.selectedRangeForDaysLate = this.totalDaysLate[0];
+
+                    this.fetchMonthlyStat();
+                },
+                error => {debugger}
+            )
+    }
+
+    fetchMonthlyStat() {
+        let startRange = this.dateUtil.getFirstDayOfCurrentMonth(new Date()).getTime();
+        let endRange = this.dateUtil.getLastDayOfCurrentMonth(new Date()).getTime();
+
+        //fetch current week
+        this.daysPresentRequest.email = this.userObj.email;
+        this.daysPresentRequest.orgId = this.userObj.orgId;
+        this.daysPresentRequest.locId = this.userObj.locId;
+        this.daysPresentRequest.currentStartTime = this.dateUtil.getStartOfDay(new Date(startRange));
+        this.daysPresentRequest.currentEndTime = this.dateUtil.getEndOfDay(new Date(endRange));
+        this.daysPresentRequest.prevStartTime = this.dateUtil.getPreviousWeekTimeStamp(this.dateUtil.getStartOfDay(new Date(startRange)) - 1)
+        this.daysPresentRequest.prevEndTime = this.dateUtil.getStartOfDay(new Date(startRange)) - 1;
+        this.reportService.getDaysPresent(this.daysPresentRequest)
+            .subscribe(
+                result => {
+                    this.totalDaysPresent[1].daysPresent = result.daysPresent;
+                    // this.totalDaysPresent[0].daysPresent = result.daysPresent;
+                    this.totalDaysEarly[1].daysEarly = result.daysEarly;
+                    this.totalDaysEarly[1].trend = result.earlyTrend;
+                    this.totalDaysLate[1].daysLate = result.daysLate;
+                    this.totalDaysLate[1].trend = result.lateTrend;
+
+                    this.selectedRangeForDaysPresent = this.totalDaysPresent[1];
+                    this.selectedRangeForDaysEarly = this.totalDaysEarly[1];
+                    this.selectedRangeForDaysLate = this.totalDaysLate[1];
+                },
+                error => {debugger}
+            )
+    }
+
+    filterDaysPresent(type?: string) {
+        if(type == "week") {
+            this.selectedRangeForDaysPresent = this.totalDaysPresent[0];
+        } else if(type == "month") {
+            this.selectedRangeForDaysPresent = this.totalDaysPresent[1];
+        }
+    }
+
+    filterDaysEarly(type?: string) {
+        if(type == "week") {
+            this.selectedRangeForDaysEarly = this.totalDaysEarly[0];
+        } else if(type == "month") {
+            this.selectedRangeForDaysEarly = this.totalDaysEarly[1];
+        }
+    }
+
+    filterDaysLate(type?: string) {
+        if(type == "week") {
+            this.selectedRangeForDaysLate = this.totalDaysLate[0];
+        } else if(type == "month") {
+            this.selectedRangeForDaysLate = this.totalDaysLate[1];
+        }
     }
 
     ngOnDestroy() {
