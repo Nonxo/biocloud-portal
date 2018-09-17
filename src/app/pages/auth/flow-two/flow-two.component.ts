5f62b6846324ebdb244c806f737a01c9aeecc355
kingsley ezeokeke <kezeokeke@seamfix.com>
2018-09-17 10:30:40 +0100

    Implement single signin/signup page

--- src/app/pages/auth/flow-two/flow-two.component.ts
+++ src/app/pages/auth/flow-two/flow-two.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
     selector: 'app-flow-two',
@@ -9,7 +10,14 @@ export class FlowTwoComponent implements OnInit {
 
     loginFlag: boolean = false;
 
-    constructor() {
+    constructor(private route: ActivatedRoute) {
+        this.route
+            .queryParams
+            .subscribe(params => {
+                    // Defaults to null if no query param provided.
+                    this.loginFlag = (params['login'] == 'true') || false;
+                }
+            )
     }
 
     ngOnInit() {
